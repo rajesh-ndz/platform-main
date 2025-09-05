ac40e359f6fb3deeac9b416b41c73b43ee5c9ad6
rajesh <rajesh.r@ndimensionz.com>
2025-09-06 01:29:53 +0530

    edit

--- infra/environments/stage/stacks/nlb/outputs.tf
+++ infra/environments/stage/stacks/nlb/outputs.tf
@@ -1,12 +1,5 @@
-output "lb_arn" {
-  value = module.nlb.lb_arn
-}
-output "lb_dns_name" {
-  value = module.nlb.lb_dns_name
-}
-output "lb_zone_id" {
-  value = module.nlb.lb_zone_id
-}
-output "listener_arns" {
-  value = module.nlb.listener_arns
-}
+output "lb_arn"              { value = module.nlb.lb_arn }
+output "lb_dns_name"         { value = module.nlb.lb_dns_name }
+output "lb_zone_id"          { value = module.nlb.lb_zone_id }
+output "listener_arns"       { value = module.nlb.listener_arns }
+output "target_group_arns"   { value = module.nlb.target_group_arns }
